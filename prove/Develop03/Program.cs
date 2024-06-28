a789fca13560ef5db7d0ed6065261b245244aebb
“Cooper Brady” <cbra207@wgu.edu>
2024-06-28 00:18:19 -0600

    update week10 group activity.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,152 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("All words are hidden. Program will end.");
+                break;
+            }
+
+            Console.WriteLine("Press Enter to hide more words or type 'quit' to exit:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
+    }
+}
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? new string('_', _text.Length) : _text;
+    }
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        return _endVerse > _verse
+            ? $"{_book} {_chapter}:{_verse}-{_endVerse}"
+            : $"{_book} {_chapter}:{_verse}";
+    }
+}
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        foreach (var word in text.Split(' '))
+        {
+            _words.Add(new Word(word));
+        }
+        _random = new Random();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        List<Word> wordsToHide = new List<Word>(_words);
+        for (int i = 0; i < numberToHide; i++)
+        {
+            if (wordsToHide.Count == 0)
+            {
+                break;
+            }
+
+            int index = _random.Next(wordsToHide.Count);
+            wordsToHide[index].Hide();
+            wordsToHide.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + "\n";
+        foreach (var word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
