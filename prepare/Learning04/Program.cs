e3c7eafa39ba06d43ddca3a0cde72ebabb7b7cf8
James Brady <bra16046@byui.edu>
2024-07-03 20:42:13 -0600

    inital commit of current progress of week4 prove assignment.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -17,3 +17,9 @@ class Program
         Console.WriteLine(assig3.GetWritingInformation());
     }
 }
+// Have a menu system to allow the user to choose an activity.
+// Each activity should start with a common starting message that provides the name of the activity, a description, and asks for and sets the duration of the activity in seconds. Then, it should tell the user to prepare to begin and pause for several seconds.
+// Each activity should end with a common ending message that tells the user they have done a good job, and pause and then tell them the activity they have completed and the length of time and pauses for several seconds before finishing.
+// Whenever the application pauses it should show some kind of animation to the user, such as a spinner, a countdown timer, or periods being displayed to the screen.
+// The interface for the program should remain generally true to the one shown in the video demo.
+// Provide activities for reflection, breathing, and enumeration, as described below:
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter your response: ");
+                    string response = Console.ReadLine();
+                    journal.AddEntry(response);
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to save: ");
+                    string saveFilename = Console.ReadLine();
+                    journal.SaveToFile(saveFilename);
+                    break;
+                case "4":
+                    Console.Write("Enter filename to load: ");
+                    string loadFilename = Console.ReadLine();
+                    journal.LoadFromFile(loadFilename);
+                    break;
+                case "5":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
     }
 }
+
+// Have a menu system to allow the user to choose an activity.
+// Each activity should start with a common starting message that provides the name of the activity, a description, and asks for and sets the duration of the activity in seconds. Then, it should tell the user to prepare to begin and pause for several seconds.
+// Each activity should end with a common ending message that tells the user they have done a good job, and pause and then tell them the activity they have completed and the length of time and pauses for several seconds before finishing.
+// Whenever the application pauses it should show some kind of animation to the user, such as a spinner, a countdown timer, or periods being displayed to the screen.
+// The interface for the program should remain generally true to the one shown in the video demo.
+// Provide activities for reflection, breathing, and enumeration, as described below:
